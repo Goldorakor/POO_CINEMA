fa5fa0f79f600b4df11072bf17e138f9d25906f1
Michaël HEID <michael123456222444666@gmail.com>
2024-10-18 00:29:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<h1>POO Banque</h1>
+   
+
+<?php
+
+// require "Entreprise.php";
+// require "Employe.php";
+
+spl_autoload_register(function ($class_name) {
+    require 'classes/'. $class_name .'.php'; // 'classes/' puisque nos fichiers de classe ont été stockés dans le dossier 'classes'
+});
+
